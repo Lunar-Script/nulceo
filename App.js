3bab46faa07005580dd43c358f0606fdc52da9fb
Lunar-Script <lunarscriptsl@gmail.com>
2022-10-16 16:24:53 +0530

    added the auto update functions to all Positions

--- App.js
+++ App.js
@@ -59,55 +59,55 @@ export default function App() {
       .database()
       .ref("zen")
       .once("value", function (snapshot) {
-        console.log(
-          "Level 1 Tower | p1: ",
-          snapshot.child("level1").child("1").hasChild("tower")
-        );
-        console.log(
-          "Level 1 Tower | p2: ",
-          snapshot.child("level1").child("2").hasChild("tower")
-        );
-        console.log(
-          "Level 1 Tower | p3: ",
-          snapshot.child("level1").child("3").hasChild("tower")
-        );
-        console.log(
-          "Level 1 Tower | p4: ",
-          snapshot.child("level1").child("4").hasChild("tower")
-        );
-        console.log(
-          "Level 1 Tower | p5: ",
-          snapshot.child("level1").child("5").hasChild("tower")
-        );
-        console.log(
-          "Level 1 Tower | p6: ",
-          snapshot.child("level1").child("6").hasChild("tower")
-        );
+        // console.log(
+        //   "Level 1 Tower | p1: ",
+        //   snapshot.child("level1").child("1").hasChild("tower")
+        // );
+        // console.log(
+        //   "Level 1 Tower | p2: ",
+        //   snapshot.child("level1").child("2").hasChild("tower")
+        // );
+        // console.log(
+        //   "Level 1 Tower | p3: ",
+        //   snapshot.child("level1").child("3").hasChild("tower")
+        // );
+        // console.log(
+        //   "Level 1 Tower | p4: ",
+        //   snapshot.child("level1").child("4").hasChild("tower")
+        // );
+        // console.log(
+        //   "Level 1 Tower | p5: ",
+        //   snapshot.child("level1").child("5").hasChild("tower")
+        // );
+        // console.log(
+        //   "Level 1 Tower | p6: ",
+        //   snapshot.child("level1").child("6").hasChild("tower")
+        // );
 
-        console.log(
-          "Level 1 wifi | p1: ",
-          snapshot.child("level1").child("1").hasChild("wifi")
-        );
-        console.log(
-          "Level 1 wifi | p2: ",
-          snapshot.child("level1").child("2").hasChild("wifi")
-        );
-        console.log(
-          "Level 1 wifi | p3: ",
-          snapshot.child("level1").child("3").hasChild("wifi")
-        );
-        console.log(
-          "Level 1 wifi | p4: ",
-          snapshot.child("level1").child("4").hasChild("wifi")
-        );
-        console.log(
-          "Level 1 wifi | p5: ",
-          snapshot.child("level1").child("5").hasChild("wifi")
-        );
-        console.log(
-          "Level 1 wifi | p6: ",
-          snapshot.child("level1").child("6").hasChild("wifi")
-        );
+        // console.log(
+        //   "Level 1 wifi | p1: ",
+        //   snapshot.child("level1").child("1").hasChild("wifi")
+        // );
+        // console.log(
+        //   "Level 1 wifi | p2: ",
+        //   snapshot.child("level1").child("2").hasChild("wifi")
+        // );
+        // console.log(
+        //   "Level 1 wifi | p3: ",
+        //   snapshot.child("level1").child("3").hasChild("wifi")
+        // );
+        // console.log(
+        //   "Level 1 wifi | p4: ",
+        //   snapshot.child("level1").child("4").hasChild("wifi")
+        // );
+        // console.log(
+        //   "Level 1 wifi | p5: ",
+        //   snapshot.child("level1").child("5").hasChild("wifi")
+        // );
+        // console.log(
+        //   "Level 1 wifi | p6: ",
+        //   snapshot.child("level1").child("6").hasChild("wifi")
+        // );
 
         // CHECIKING AND UPDATING THE WIFI VALUES ACCORDING TO TOWER VALUES
 
@@ -187,6 +187,396 @@ export default function App() {
         }
 
         //L1P2 end
+
+        //L1P3 begin
+
+        if (snapshot.child("level1").child("3").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level1")
+              .child("3")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L1P3");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level1")
+              .child("3")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level1").child("3").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level1").child("3").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L1P3!");
+        }
+
+        //L1P3 end
+
+        //L1P4 begin
+
+        if (snapshot.child("level1").child("4").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level1")
+              .child("4")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L1P4");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level1")
+              .child("4")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level1").child("4").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level1").child("4").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L1P4!");
+        }
+
+        //L1P4 end
+
+        //L1P5 begin
+
+        if (snapshot.child("level1").child("5").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level1")
+              .child("5")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L1P5");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level1")
+              .child("5")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level1").child("5").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level1").child("5").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L1P5!");
+        }
+
+        //L1P5 end
+
+        //L1P6 begin
+
+        if (snapshot.child("level1").child("6").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level1")
+              .child("6")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L1P6");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level1")
+              .child("6")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level1").child("6").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level1").child("6").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L1P6!");
+        }
+
+        //L1P6 end
+
+        //L2P1 begin
+
+        if (snapshot.child("level2").child("1").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level2")
+              .child("1")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L2P1");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level2")
+              .child("1")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level2").child("1").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level2").child("1").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L2P1!");
+        }
+
+        //L2P1 end
+
+        //L2P2 begin
+
+        if (snapshot.child("level2").child("2").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level2")
+              .child("2")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L2P2");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level2")
+              .child("2")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level2").child("2").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level2").child("2").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L2P2!");
+        }
+
+        //L2P2 end
+
+        //L2P3 begin
+
+        if (snapshot.child("level2").child("3").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level2")
+              .child("3")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L2P3");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level2")
+              .child("3")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level2").child("3").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level2").child("3").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L2P3!");
+        }
+
+        //L2P3 end
+
+        //L2P4 begin
+
+        if (snapshot.child("level2").child("4").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level2")
+              .child("4")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L2P4");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level2")
+              .child("4")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level2").child("4").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level2").child("4").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L2P4!");
+        }
+
+        //L2P4 end
+
+        //L2P5 begin
+
+        if (snapshot.child("level2").child("5").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level2")
+              .child("5")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L2P5");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level2")
+              .child("5")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level2").child("5").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level2").child("5").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L2P5!");
+        }
+
+        //L2P5 end
+
+        //L2P6 begin
+
+        if (snapshot.child("level2").child("6").hasChild("tower") === true) {
+          if (
+            snapshot
+              .child("refresh")
+              .child("level2")
+              .child("6")
+              .hasChild("restarted") === true
+          ) {
+            console.log("Already Restarted at L2P6");
+          } else {
+            firebase
+              .database()
+              .ref("zen")
+              .child("refresh")
+              .child("level2")
+              .child("6")
+              .update({
+                restarted: 1,
+              });
+            Restart();
+          }
+
+          if (snapshot.child("level2").child("6").hasChild("wifi") === true) {
+            console.log("Wifi value already exists!");
+          } else {
+            setTimeout(() => {
+              firebase.database().ref("zen").child("level2").child("6").update({
+                wifi: wifi,
+              });
+            }, 1500);
+          }
+        } else {
+          console.log("No tower values detected at L2P6!");
+        }
+
+        //L2P6 end
       });
   }, 5000);
 
